bc6351bcf6609cd16ae70cc8933307ddfc189b09
Devyesh <60972054+Devyesh@users.noreply.github.com>
2020-03-01 17:33:39 +0530

    Add files via upload

---
+++ lovejs.js
@@ -0,0 +1,28 @@
+	function disp() {
+		var male=document.f1.m1.value;
+		var female=document.f1.fm1.value;
+		if(male=="")
+		{
+			alert("Enter a valid boy name");
+			document.f1.m1.focus();
+			exit();
+		}
+		if(female=="")
+		{
+			alert("Enter a valid girl name");
+			document.f1.mf1.focus();
+			exit();
+		}
+		if(male==female)
+		{
+			alert("Love is 100% ");
+			exit();
+		}
+		else
+		{
+		var r=Math.random();
+		r=r*100;
+		r=Math.ceil(r);
+		alert("Love is "+r);
+		}
+		}
